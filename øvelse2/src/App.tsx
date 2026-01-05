79abc3a49068628fdf9514249e2c4b65f6c345bf
Kari idskov holm Madsen <kama7@elev.techcollege.dk>
2026-01-05 14:04:33 +0100

    v2

---
+++ øvelse2/src/App.tsx
@@ -0,0 +1,20 @@
+import { Header } from './Components/Header/Header'
+import { Main } from './Components/Main/Main'
+import { Button } from './Components/Button/Button'
+import { Footer } from './Components/Footer/Footer'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <Header></Header>
+    <Main></Main>
+    <Button></Button>
+    <Footer></Footer>
+      
+    </>
+  )
+}
+
+export default App
---
+++ øvelse2/src/Components/Footer/Footer.tsx
@@ -0,0 +1,4 @@
+export const Footer = () => {
+
+            return <footer>Her er en footer</footer>;
+    }
