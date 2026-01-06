81f6c1ec1dc58c03a574597436b261296fc49ab3
Kari idskov holm Madsen <kama7@elev.techcollege.dk>
2026-01-06 12:16:11 +0100

    v4

--- øvelse2/src/Components/NavBar/NavBar.tsx
+++ øvelse2/src/Components/NavBar/NavBar.tsx
@@ -1,11 +1,14 @@
+import style from './NavBar.module.scss'
 export const NavBar = () => {
 
             return (
         <>
             <nav>
+                <ul className={style.ul}>
                 <li><a href="#">Hjem</a></li>
                 <li><a href="#">Hvem er vi?</a></li>
                 <li><a href="#">Åbningstider</a></li>
+        </ul>
             </nav>
         </>
             )};
