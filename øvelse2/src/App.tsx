ad16886d5cc6330e4c47076404dc88c4490b3f78
Kari idskov holm Madsen <kama7@elev.techcollege.dk>
2026-01-07 18:16:04 +0100

    v6

--- øvelse2/src/App.tsx
+++ øvelse2/src/App.tsx
@@ -4,19 +4,16 @@ import { Main } from './Components/MainPage/MainPage'
 import { Button } from './Components/Button/Button'
 import { Footer } from './Components/Footer/Footer'
 import {Greetings} from './Components/Greetings/Greetings'
-import {Wrapper} from './Components/Wrapper/Wrapper'
-
 import './App.css'
 
 function App() {
-  
+ 
   return (
     <>
     <NavBar></NavBar>
     <Header></Header>
     <Main></Main>
     <Greetings></Greetings>
-    {/* <Wrapper></Wrapper> */}
     <Button></Button>
     <Footer></Footer>
        
--- øvelse2/src/Components/Wrapper/Wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import style from './Wrapper.module.scss'
-
-interface WrapperInterface {
-  children: React.ReactNode
-}
-
-export function Wrapper({ children }: WrapperInterface) {
-  return <main className={style.wrapperClass}>{children}</main>
-}
